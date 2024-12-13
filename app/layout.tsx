c7fe2199b5ff06404fd51518d621ef4357008620
Promise Duke <promiseduke@gmail.com>
2024-12-13 08:07:28 +0100

    favicon updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,24 +2,28 @@ import './globals.css';
 import Navbar from './components/Navbar/index';
 import Footer from './components/Footer/index';
 
-
 export const metadata = {
-  title: 'Dev Champions ',
+  title: 'Dev Champions',
   description: 'Active for all Software related activities',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.jpg" type="image/x-icon" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </head>
       <body>
         <Navbar />
         {children}
         <Footer />
       </body>
     </html>
-  )
+  );
 }
