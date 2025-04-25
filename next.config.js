fc7295e784c7c079e0b74d6827c2d9b28b79bac2
Promise Duke <promiseduke@gmail.com>
2025-04-25 13:05:56 +0100

    Next Config Redirection Setup

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-}
-
-module.exports = nextConfig
+    async redirects() {
+      return [
+        {
+          source: '/jobs', // Redirect for /jobs
+          destination: 'https://jobs.dev-champions.tech', // External URL
+          permanent: true, // Permanent redirect (308)
+        },
+        {
+          source: '/blogs', // Redirect for /blogs
+          destination: 'https://blogs.dev-champions.tech', // External URL
+          permanent: true, // Permanent redirect (308)
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
