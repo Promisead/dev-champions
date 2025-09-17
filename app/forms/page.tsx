e88510f04eeea3919f741eb5ff0dd1e9d779327a
Promise Duke <77851203+Promisead@users.noreply.github.com>
2025-09-17 19:23:09 +0100

    Update page.tsx
    200 google form fixed

--- app/forms/page.tsx
+++ app/forms/page.tsx
@@ -10,7 +10,7 @@ const Forms: React.FC = () => {
   // Mapping of form IDs to their respective URLs
   const formUrls: { [key: string]: string } = {
     Lv100: "https://forms.gle/Z914aX9M2LxqHZn36",
-    Lv200: "https://docs.google.com/forms/d/e/1FAIpQLSdB63jWSJtJ8hTd73SBhdhvXZhIOY5_MUeCSY43MJCmfcp7Nw/viewform?usp=header",
+    Lv200: "https://docs.google.com/forms/d/e/1FAIpQLSdB63jWSJtJ8hTd73SBhdhvXZhIOY5_MUeCSY43MJCmfcp7Nw/viewform?embedded=true",
     Lv300: "https://docs.google.com/forms/d/e/1FAIpQLScHjhmG0aERsAQjzVT4C8EvKyIFSL_HbXzipr8wYDllCWSDBA/viewform?embedded=true",
     Lv400: "https://forms.gle/XW9o4MNfSEzMpc4v9",
     Others: "https://forms.gle/Xt3FMBVqkv7Tm5Kr6",
